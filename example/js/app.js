98861e16351a6747b804124ff2689eba6a30748b
Phillip Moon <pmoon@Phillips-MacBook-Pro-2.local>
2016-12-24 13:39:32 +1300

    Added more to example code

---
+++ example/js/app.js
@@ -0,0 +1,11 @@
+(function () {
+	var _app = window.app = {};
+
+	//private functions
+	function init() {
+		console.log("Trump has initialised");
+	}
+
+	//public functions
+	_app.init = init;
+})();
